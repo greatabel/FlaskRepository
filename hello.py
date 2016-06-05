7411e16b3edd084b8a47dab2334edbf3c7cfc46b
greatabel <greatabel1@126.com>
2016-06-06 01:11:16 +0800

    test

--- hello.py
+++ hello.py
@@ -11,7 +11,10 @@ site.addsitedir('/root/.virtualenvs/env1/lib/python3.4/site-packages')
 # Activate your virtual env
 activate_env=os.path.expanduser("/root/.virtualenvs/env1/bin/activate_this.py")
 # execfile(activate_env, dict(__file__=activate_env))
-exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), dict(__file__=activate_env))
+# exec(compile(open(activate_env, "r").read(), activate_env, 'exec'), dict(__file__=activate_env))
+with open(activate_env) as f:
+    code = compile(f.read(), activate_env, 'exec')
+    exec(code, dict(__file__=activate_env))
 
 import feedparser
 app = Flask(__name__)
