37d3c3ed638db1f775c7e9aa39e68804a9c6dadb
greatabel <greatabel1@126.com>
2017-01-20 22:43:22 +0800

    add_crime mock

--- ch7google_maps_for_crime/mockdbhelper.py
+++ ch7google_maps_for_crime/mockdbhelper.py
@@ -2,3 +2,8 @@ class MockDBHelper:
 
     def connect(self, database="crimemap"):
         pass
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        data = [category, date, latitude, longitude, description]
+        for i in data:
+            print(i, type(i))
