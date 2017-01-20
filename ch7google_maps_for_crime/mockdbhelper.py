fbd0f069bf1b7fc0ff8c7bb1ebe97c3ed0735fb8
greatabel <greatabel1@126.com>
2017-01-20 22:44:04 +0800

    get_all_crimes

--- ch7google_maps_for_crime/mockdbhelper.py
+++ ch7google_maps_for_crime/mockdbhelper.py
@@ -7,3 +7,16 @@ class MockDBHelper:
         data = [category, date, latitude, longitude, description]
         for i in data:
             print(i, type(i))
+
+    def get_all_crimes(self):
+        return [{'latitude': -33.301304,
+                 'longitude': 26.523355,
+                 'date': "2000-01-01",
+                 'category': "mugging",
+                 'description': "mock description"}]
+
+    def add_input(self, data):
+        pass
+
+    def clear_all(self):
+        pass
