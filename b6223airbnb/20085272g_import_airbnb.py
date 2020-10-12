95ae86a69415922ca40b8b92e96e028d0b84a3bf
greatabel <myreceiver2for2github@gmail.com>
2020-10-12 21:57:56 +0800

    4tables insert

--- b6223airbnb/20085272g_import_airbnb.py
+++ b6223airbnb/20085272g_import_airbnb.py
@@ -58,13 +58,107 @@ def create_table_schema():
 
 
 def get_json(filename):
-    file = 'airbnb.json';
+    file = 'airbnb.json'
     listing = []
-    with open(file, 'r',encoding="utf8") as myfile:
-        data=myfile.read()
+    with open(file, 'r', encoding="utf8") as myfile:
+        data = myfile.read()
         listing = json.loads(data)
+    return listing
+
+
+def import_accommodation(listing):
+    insert_list = []
+    conn = sqlite3.connect("airbnb.db")
+    c = conn.cursor()
+    for i in listing:
+        id = i["_id"]
+        name = i["name"]
+        summary = i["summary"]
+        url = i["listing_url"]
+        insert_list.append((id, name, summary, url))
+
+    c.executemany("INSERT INTO accommodation (id, name, summary, url)\
+                   VALUES (?, ?, ?, ?)", insert_list)
+    conn.commit()
+    conn.close()
+    print('insert ', len(insert_list), ' accommodation')
+
+
+def import_host(listing):
+    insert_list = []
+    host_count_dict = {}
+    conn = sqlite3.connect("airbnb.db")
+    c = conn.cursor()
+    for i in listing:
+        h = i["host"]
+
+        host_count = host_count_dict.get(h['host_id'], None)
+            # consider reviewer_id and reveiwer_name combined should be unique,
+            # we only store the 1st occurrence
+        if host_count is None:
+            host_count_dict[h['host_id']] = 1
+            insert_list.append((h['host_id'], h['host_url'], h['host_name'],
+                                h['host_about'], h['host_location']))
+        else:
+            print('## host exitsted', len(insert_list))
+
+    c.executemany("INSERT INTO host (host_id, host_url, host_name, host_about, host_location)\
+                   VALUES (?, ?, ?, ?, ?)", insert_list)
+    conn.commit()
+    conn.close()
+    print('insert ', len(insert_list), ' hosts')
+
+
+def import_reviewer(listing):
+    insert_list = []
+    conn = sqlite3.connect("airbnb.db")
+    c = conn.cursor()
+    for i in listing:
+        reviews = i["reviews"]
+        for review in reviews:
+            # consider reviewer_id and reveiwer_name combined should be unique,
+            # we only store the 1st occurrence
+            if (review['reviewer_id'], review['reviewer_name']) not in insert_list:
+                insert_list.append((review['reviewer_id'], review['reviewer_name']))
+
+    c.executemany("INSERT INTO reviewer (rid, rname)\
+                   VALUES (?, ?)", insert_list)
+    conn.commit()
+    conn.close()
+    print('insert ', len(insert_list), ' reviewers')
+
+
+def import_review(listing):
+    insert_list = []
+    reviewer_count_dict = {}
+    conn = sqlite3.connect("airbnb.db")
+    c = conn.cursor()
+    for i in listing:
+        id = i["_id"]
+        reviews = i["reviews"]
+        for review in reviews:
+            reviewer_count = reviewer_count_dict.get(review['reviewer_id'], None)
+            # If a reviewer (with certain reviewer ID) have more than one reviews,
+            # only the first occurrence of the reviewer data will be stored in the database.
+            if reviewer_count is None:
+                d = review['date']['$date']
+                # print('d=', d)
+                reviewer_count_dict[review['reviewer_id']] = 1
+                insert_list.append((review['reviewer_id'], review['comments'], d, id))
+            # else:
+            #     print('allready exitsted reviewer', len(insert_list))
+
+    c.executemany("INSERT INTO review (rid, comment, datetime, accommodation_id)\
+                   VALUES (?, ?, ?, ?)", insert_list)
+    conn.commit()
+    conn.close()
+    print('insert ', len(insert_list), ' reviews')
 
 
 if __name__ == "__main__":
     create_table_schema()
-    get_json('airbnb.json')
+    listing = get_json('airbnb.json')
+    import_accommodation(listing)
+    import_host(listing)
+    import_reviewer(listing)
+    import_review(listing)
