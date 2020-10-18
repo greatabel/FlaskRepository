d0518b70b0ef0af4aab59837d4e6e2fedb1149b5
greatabel <myreceiver2for2github@gmail.com>
2020-10-19 04:11:59 +0800

    search

--- b6427legaltext/app.py
+++ b6427legaltext/app.py
@@ -22,6 +22,15 @@ def insert_db(sql, p):
     conn.close()
 
 
+def select_db(sql):
+    viewdata = []
+    with sqlite3.connect("legaltext.db") as db:
+        cursor = db.cursor()    
+        cursor.execute(sql)
+        viewdata = cursor.fetchall()
+    db.commit()
+    return viewdata
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -95,6 +104,86 @@ def add():
     return render_template('add.html', typelist=typelist,
                  errors=errors, result=result)
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        select_Items = request.form['select_Items']   
+        p = None
+        sql = None
+        if select_Items == 'tb1':
+            case_court = request.form['case_court'] 
+            case_year = request.form['case_year']
+            case_id = request.form['case_id']
+            case_cause = request.form['case_cause']
+            case_industry = request.form['case_industry']
+            sql =  """select title from reference where """
+            if case_court:
+                sql += """ case_court='""" + case_court + """' """
+            if case_year:
+                sql += """ case_year='""" + case_year + """' """
+            if case_id:
+                sql += """ and case_id='""" + case_id + + """' """
+            if case_cause:
+                sql += """ and case_cause='""" + case_cause + + """' """
+            if case_industry:
+                sql += """ and case_id='""" + case_industry + + """' """
+
+
+        elif select_Items == 'tb2':
+            memo_issue = request.form['memo_issue']
+            memo_year = request.form['memo_year']
+            meomo_industry = request.form['meomo_industry']
+            sql =  """select title from memo where """
+            if memo_issue:
+                sql += """ memo_issue='""" + memo_issue + """' """
+            if memo_year:
+                sql += """ memo_year='""" + memo_year + """' """
+            if meomo_industry:
+                sql += """ and meomo_industry='""" + meomo_industry + + """' """
+
+        elif select_Items == 'tb3':
+            evidence_cause = request.form['evidence_cause']
+            evidence_plaintiff = request.form['evidence_plaintiff']
+            evidence_nation = request.form['evidence_nation']
+            evidence_industry = request.form['evidence_industry']
+            evidence_court = request.form['evidence_court']
+            sql =  """select title from evidence where """
+            if evidence_cause:
+                sql += """ evidence_cause='""" + evidence_cause + """' """
+            if evidence_plaintiff:
+                sql += """ evidence_plaintiff='""" + evidence_plaintiff + """' """
+            if evidence_nation:
+                sql += """ and evidence_nation='""" + evidence_nation + + """' """
+            if evidence_industry:
+                sql += """ and evidence_industry='""" + evidence_industry + + """' """
+            if evidence_court:
+                sql += """ and evidence_court='""" + evidence_court + + """' """
+
+        elif select_Items == 'tb4':
+            contract_type = request.form['contract_type']
+            contract_language = request.form['contract_language']
+            contract_amount = request.form['contract_amount']
+            contract_position = request.form['contract_position']
+            contract_industry = request.form['contract_industry']
+            sql =  """select title from contract where """
+            if contract_type:
+                sql += """ contract_type='""" + contract_type + """' """
+            if contract_language:
+                sql += """ contract_language='""" + contract_language + """' """
+            if contract_amount:
+                sql += """ and contract_amount='""" + contract_amount + + """' """
+            if contract_position:
+                sql += """ and contract_position='""" + contract_position + + """' """
+            if contract_industry:
+                sql += """ and contract_industry='""" + case_industry + + """' """
+        print(sql, '#'*5)
+        results  = select_db(sql)
+        return render_template('search.html', typelist=typelist,
+                     errors=errors, result=results)
 
 if __name__ == '__main__':
     app.run()
