1a81304558c80b5e8104b2451c91c84f08f06add
greatabel <myreceiver2for2github@gmail.com>
2020-10-19 01:43:57 +0800

    frontEnd

--- b6427legaltext/app.py
+++ b6427legaltext/app.py
@@ -7,15 +7,25 @@ app.debug = True
 app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = False
 
-typelist = ['法律意见书','参考案例', '合同文本', '证据目录']
+typelist = ['参考案例', '法律意见书', '证据目录', '合同文本']
 
 
 @app.route('/add')
 def add():
-    # input :http://www.baidu.com
-    query_value = request.args.get('query')
-    index =  " is here"
-    return render_template('add.html', typelist=typelist,index=index)
+    errors = []
+    result = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('add.html', typelist=typelist,
+        result=result)
 
 
 if __name__ == '__main__':
