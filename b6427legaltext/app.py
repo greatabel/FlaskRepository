54d2ce148e3f32ad8577df274441636254cc5ac7
greatabel <myreceiver2for2github@gmail.com>
2020-10-19 03:26:43 +0800

    add

--- b6427legaltext/app.py
+++ b6427legaltext/app.py
@@ -13,7 +13,8 @@ typelist = ['参考案例', '法律意见书', '证据目录', '合同文本']
 
 
 def insert_db(sql, p):
-
+    print('#'*5, p, len(p))
+    print('@'*5, sql)
     conn = sqlite3.connect("legaltext.db")
     c = conn.cursor()
     c.execute(sql, p)
@@ -25,17 +26,18 @@ def insert_db(sql, p):
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     errors = []
-    result = {}
+    result = None
     if request.method == "POST":
         # get url that the user has entered
         p = None
         sql = None
 
         select_Items = request.form['select_Items']
-        title = request.form['title']
-        content = request.form['content']
+
 
         if select_Items == 'tb1':
+            title = request.form['title1']
+            content = request.form['content1']
             case_court = request.form['case_court']
             case_year = request.form['case_year']
             case_id = request.form['case_id']
@@ -44,14 +46,44 @@ def add():
             p = (title, content, case_court, case_year, case_id, case_cause, case_industry)
             sql = "INSERT INTO reference (title, content, case_court, case_year, case_id, case_cause, case_industry)\
                     VALUES (?, ?, ?, ?, ?, ?, ?)"
-
         elif select_Items == 'tb2':
-            ''
+            title = request.form['title2']
+            content = request.form['content2']
+            memo_issue = request.form['memo_issue']
+            memo_year = request.form['memo_year']
+            meomo_industry = request.form['meomo_industry']
+            p = (title, content, memo_issue, memo_year, meomo_industry)
+            sql = "INSERT INTO memo (title, content, memo_issue, memo_year, meomo_industry)\
+                    VALUES (?, ?, ?, ?, ?)"
         elif select_Items == 'tb3':
-            ''
+            title = request.form['title3']
+            content = request.form['content3']
+            evidence_cause = request.form['evidence_cause']
+            evidence_plaintiff = request.form['evidence_plaintiff']
+            evidence_nation = request.form['evidence_nation']
+            evidence_industry = request.form['evidence_industry']
+            evidence_court = request.form['evidence_court']
+            p = (title, content, evidence_cause, evidence_plaintiff, evidence_nation, evidence_industry, evidence_court)
+            sql = "INSERT INTO evidence (title, content, evidence_cause, evidence_plaintiff, evidence_nation, evidence_industry, evidence_court)\
+                    VALUES(?, ?, ?, ?, ?, ?, ?)"
+
+
+
         elif select_Items == 'tb4':
-            ''
+            title = request.form['title4']
+            content = request.form['content4']
+            contract_type = request.form['contract_type']
+            contract_language = request.form['contract_language']
+            contract_amount = request.form['contract_amount']
+            contract_position = request.form['contract_position']
+            contract_industry = request.form['contract_industry']
+            p = (title, content, contract_type, contract_language, contract_amount, contract_position, contract_industry)
+            sql = "INSERT INTO contract (title, content, contract_type, contract_language, contract_amount, contract_position, contract_industry)\
+                    VALUES(?, ?, ?, ?, ?, ?, ?)"
+
+
         insert_db(sql, p)
+        result = '数据插入成功'
         # try:
         #     url = request.form['url']
         #     r = requests.get(url)
