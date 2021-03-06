8aac1de644ec64448992310aeac1dc787f1c49ae
greatabel <myreceiver2for2github@gmail.com>
2021-03-07 02:46:59 +0800

    format all py

--- b9655_movie_recommendsystem/app/admin/__init__.py
+++ b9655_movie_recommendsystem/app/admin/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-admin = Blueprint('admin',__name__)
+admin = Blueprint("admin", __name__)
 
 import app.admin.views
-
--- b9655_movie_recommendsystem/manage.py
+++ b9655_movie_recommendsystem/manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager
 
 # manage = Manager(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # manage.run()
     app.run()
     # app.run(port=8080)
