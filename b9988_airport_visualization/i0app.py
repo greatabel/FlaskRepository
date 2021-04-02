3bd5cecb210097e854b4b6f73396c65d1768b283
greatabel <myreceiver2for2github@gmail.com>
2021-04-02 16:41:13 +0800

    relationship of airpoint

--- b9988_airport_visualization/i0app.py
+++ b9988_airport_visualization/i0app.py
@@ -1,6 +1,10 @@
+import os
+import json
+
 from flask import Flask, request
 from flask import render_template
 from flask import make_response
+from flask import jsonify
 
 import csv
 from termcolor import colored
@@ -18,12 +22,12 @@ def load(filepath):
 app = Flask(__name__)
 app.debug = True
 
-# 目标id， 目标name相似度， 目标bio相似度，目标头像相似度
-mydict = {
-    1: [3, 1.0, 0.391, 0.685],
-    2: [2, 0.0, 0.257, 0.604],
-    3: [2, 0.0, 0.257, 0.604]
-}
+
+# mydict = {
+#     1: [3, 1.0, 0.391, 0.685],
+#     2: [2, 0.0, 0.257, 0.604],
+#     3: [2, 0.0, 0.257, 0.604]
+# }
 
 @app.route('/find_connect/')
 @app.route('/find_connect/<index_id>')
@@ -40,9 +44,30 @@ def scp2(index_id=""):
 
     return r
 
+@app.route('/index/')
+def index():
+    r = make_response(
+        render_template('index.html')
+        )
 
+    return r
 
+@app.route('/index_b/')
+def index_b():
+    r = make_response(
+        render_template('index-B.html')
+        )
 
+    return r
+
+@app.route("/statistics", methods=['GET'])
+def relationship():
+    # static/data/test_data.json
+    filename = os.path.join(app.static_folder, 'data/data.json')
+    with open(filename) as test_file:
+        d = json.load(test_file)
+    print(type(d), '#'*10, d)
+    return jsonify(d)
 
 if __name__ == '__main__':
     app.run()
