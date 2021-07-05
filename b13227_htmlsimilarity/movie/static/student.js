c6c86115f99e0d62e58c613fb320bd3a37ef731c
greatabel <myreceiver2for2github@gmail.com>
2021-07-05 19:17:09 +0800

    change student.js

--- b13227_htmlsimilarity/movie/static/student.js
+++ b13227_htmlsimilarity/movie/static/student.js
@@ -1,3 +1,4 @@
 var DATA = {
-'data2021':5, 'data2020':4,'total_sentiment_polarity':-0.45, 'average_sentiment_polarity':-0.05,'num_positive':153, 'num_neural':204,'num_nagtive':153, 'data2021':3 };
-var RELATED_WORDS = {'senkaku':8,'japan':4,'china':4,'japanese territory':2,'taiwan':2, };
+'not_pass':5, 'pass':4 ,'num_positive':3, 'num_neural':1,'num_nagtive':3 };
+
+// var RELATED_WORDS = {'senkaku':8,'japan':4,'china':4,'japanese territory':2,'taiwan':2, };
