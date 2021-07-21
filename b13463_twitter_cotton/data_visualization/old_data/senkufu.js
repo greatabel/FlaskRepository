076a3a58a8033ed4536ac432a965af91f8fa7cd5
greatabel <myreceiver2for2github@gmail.com>
2021-07-22 00:38:28 +0800

    mv a13332

--- b13463_twitter_cotton/data_visualization/old_data/senkufu.js
+++
@@ -1,4 +0,0 @@
-var DATA = {
-'data2020':717, 'data2019':630,'total_sentiment_polarity':1.63, 'average_sentiment_polarity':0.03,'num_positive':1515, 'num_neural':3939,'num_nagtive':404, 'data2019':630 };
-var RELATED_WORDS = {'senkaku':47,'japan':32,'china':20,'senkaku_islands':13,'meiji':6,'japanese territory':5,'international law':3,'taiwan':3,'tensions simmer':3,'amti':3, };
-var WHO_TWEETS = {'swa011':13,'ひろちい0':8,'フルベビアイス3':3,'AMTI':3,'Japan Sea':2,'truth_jpn':2,'🇯🇵日本 心太郎':2, };
--- b13463_twitter_cotton/data_visualization/old_data/taiwan_china.js
+++
@@ -1,4 +0,0 @@
-var DATA = {
-'data2020':3478, 'data2019':3329,'total_sentiment_polarity':49.36, 'average_sentiment_polarity':0.18,'num_positive':16362, 'num_neural':5656,'num_nagtive':6161, 'data2019':3329 };
-var RELATED_WORDS = {'india':35,'ccp':25,'korea':25,'japan':23,'hong kong':23,'vietnam':22,'australia':19,'hk':13,'taiwanese':12,'trump':12, };
-var WHO_TWEETS = {'KUMAR KAUSHLENDRA':4,'😷missing Michael Kovrig':4,'Ghost_Of_Vincent_Chin':3,'WithoutFear':3,'Chu Got Leung':2,'archangel🇨🇦🍁🏒😇':2,'kiem123456789':2,'magenta magenta':2, };
