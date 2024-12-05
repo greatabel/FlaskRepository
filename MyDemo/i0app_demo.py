99ce431c6abe3dd49271d017f6e4fcac10e6df46
greatabel <myreceiver2for2github@gmail.com>
2024-12-05 15:32:35 +0800

    atler with ip

--- MyDemo/i0app_demo.py
+++ MyDemo/i0app_demo.py
@@ -1,11 +1,14 @@
-# app.py
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    hostname = socket.gethostname()  # 获取主机名
+    ip = socket.gethostbyname(hostname)  # 获取IP地址
+    return f'Hello World from {hostname} (IP: {ip})'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
